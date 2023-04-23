69124d21a529932c3c163e6b54025593034a9354
Simpath <hashemi.rahim@gmail.com>
2023-04-23 19:01:38 +0430

    added and design to react

--- src/main/java/com/example/springBootReact/student/StudentController.java
+++ src/main/java/com/example/springBootReact/student/StudentController.java
@@ -5,15 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @RestController
-    @RequestMapping(path = "api/students")
+@RequestMapping(path = "api/students")
 public class StudentController {
 
     @GetMapping
     public List<Student> getAllStudents() {
+        System.out.println("StudentController.getAllStudents " + new Timestamp(new Date().getTime()));
         return Arrays.asList(new Student(1L, "Rahim", "hashemi@gmail.com", Gender.MALE),
                 new Student(2L, "Majid", "majid@gmail.com", Gender.MALE)
         );
